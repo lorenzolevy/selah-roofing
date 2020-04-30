a7bfdc170c124382a65e22f16902937b4cc30b51
Lorenzo Levy <lorenzoroccolevy@gmail.com>
2020-04-29 23:57:51 -0700

    formspree test

--- src/pages/quote.js
+++ src/pages/quote.js
@@ -1,17 +1,27 @@
 import React from "react"
-import Hero from "../components/hero"
+
 import Layout from "../layouts/layout"
 import SEO from "../components/seo"
 
 const QuotePage = () => (
   <Layout>
     <SEO title="Quote" />
-    <form name="quote" method="POST" data-netlify="true">
-        <input type="text" name="name" id="name" placeholder="John Doe" />
-        <input type="email" name="email" placeholder="johndoe@gmail.com" />
-        <textarea type="text" name="message" placeholder="Enter Message Text" />
-        <button type="submit">Send</button>
-    </form>
+    <form
+  action="https://formspree.io/mvowplao"
+  method="POST"
+>
+  <label>
+    Your email:
+    <input type="text" name="_replyto" />
+  </label>
+  <label>
+    Your message:
+    <textarea name="message"></textarea>
+  </label>
+
+
+  <button type="submit">Send</button>
+</form>
   </Layout>
 )
 

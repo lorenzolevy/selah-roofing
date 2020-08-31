b6309597acf4661f174b3aeab6c2b625196ddc72
Lorenzo Levy <lorenzoroccolevy@gmail.com>
2020-08-31 12:12:49 -0700

    Font changes

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,40 +1,38 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from "react"
 import Img from "gatsby-background-image"
 import styled from "styled-components"
-import FsLightbox from 'react-image-lightbox'
-import "react-image-lightbox/style.css";
-
+import FsLightbox from "react-image-lightbox"
+import "react-image-lightbox/style.css"
 
 const Grid = styled.div`
-    @media (max-width: 680px) {
-      > div > div {
-        height: 270px;
-        margin-bottom: 3px;
-      }
+  @media (max-width: 680px) {
+    > div > div {
+      height: 270px;
+      margin-bottom: 3px;
     }
-    @media (max-width: 1150px) and (min-width: 680px) {
-        grid-template-columns: repeat(2, 1fr);
-        > div > div {
-          height: 300px;
-        }
-
+  }
+  @media (max-width: 1150px) and (min-width: 680px) {
+    grid-template-columns: repeat(2, 1fr);
+    > div > div {
+      height: 300px;
     }
-    @media (min-width: 1150px) {
-        grid-template-columns: repeat(3, 1fr);
-        > div > div {
-          height: 450px;
-        }
+  }
+  @media (min-width: 1150px) {
+    grid-template-columns: repeat(3, 1fr);
+    > div > div {
+      height: 450px;
     }
-    display: grid;
-    > div {
-      position: relative;
-      overflow: hidden!important;
-    }  
-    max-width: 2000px;
-    margin: 0 auto;
+  }
+  display: grid;
+  > div {
+    position: relative;
+    overflow: hidden !important;
+  }
+  max-width: 2000px;
+  margin: 0 auto;
 `
 const CardWrap = styled.div`
-    overflow: hidden;
+  overflow: hidden;
 `
 
 const ProjectCard = styled(Img)`
@@ -62,77 +60,96 @@ const CardInner = styled.div`
   flex-direction: column;
   justify-content: flex-end;
   padding-left: 1rem;
-
+  font-family: Roboto;
   &:hover {
     background-color: none;
   }
 
   > h1 {
     margin: 0;
-    padding: 1rem 1rem .2rem 1rem;
+    padding: 1rem 1rem 0.2rem 1rem;
     font-size: 1.4rem;
-    color: hsla(210,73%,95%,1);
+    color: hsla(210, 73%, 95%, 1);
   }
   > p {
-    padding: .3rem 1rem;
+    padding: 0.3rem 1rem;
     font-size: 1rem;
-    color: hsla(210,20%,80%,1);
-    font-family: Rubik;
+    color: hsla(210, 20%, 80%, 1);
+    font-family: Roboto;
+    font-weight: 300;
   }
 `
 
-
-
 export default class Gallery extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       showLightbox: false,
       imageIndex: 0,
       listIndex: 0,
-  };
-}
+    }
+  }
   render() {
-    const { data } = this.props;
-    const { showLightbox, imageIndex, listIndex } = this.state;
-    const galsList = data.wpcontent.projects.nodes.map(node=>node.projectFields.gallery.map(url=>url.sourceUrl));
+    const { data } = this.props
+    const { showLightbox, imageIndex, listIndex } = this.state
+    const galsList = data.wpcontent.projects.nodes.map(node =>
+      node.projectFields.gallery.map(url => url.sourceUrl)
+    )
     return (
-     <Fragment>
-       <Grid>
-          {data.wpcontent.projects.nodes.map((node, index) =>
-          <CardWrap 
-            key={node.projectFields.title}
-            onClick={() => this.setState({ showLightbox: true, imageIndex: index, listIndex: 0 })}
+      <Fragment>
+        <Grid>
+          {data.wpcontent.projects.nodes.map((node, index) => (
+            <CardWrap
+              key={node.projectFields.title}
+              onClick={() =>
+                this.setState({
+                  showLightbox: true,
+                  imageIndex: index,
+                  listIndex: 0,
+                })
+              }
             >
-            <ProjectCard fluid={node.projectFields.image.imageFile.childImageSharp.fluid}>
-              <CardInner>
-                <h1>{node.projectFields.title}</h1>
-                <p>{node.projectFields.description}</p>
-              </CardInner>
-            </ProjectCard>
-          </CardWrap>
-          )}
+              <ProjectCard
+                fluid={node.projectFields.image.imageFile.childImageSharp.fluid}
+              >
+                <CardInner>
+                  <h1>{node.projectFields.title}</h1>
+                  <p>{node.projectFields.description}</p>
+                </CardInner>
+              </ProjectCard>
+            </CardWrap>
+          ))}
         </Grid>
         {showLightbox && (
-        <FsLightbox
-          mainSrc={galsList[imageIndex][listIndex]}
-          nextSrc={galsList[imageIndex][(listIndex + 1) % galsList[imageIndex].length]}
-          prevSrc={galsList[imageIndex][(listIndex + galsList[imageIndex].length - 1) % galsList[imageIndex].length]}
-          onCloseRequest={() => this.setState({ showLightbox: false })}
-          onMovePrevRequest={() =>
-            this.setState({
-              listIndex: (listIndex + galsList[imageIndex].length - 1) % galsList[imageIndex].length
-            })
-          }
-          onMoveNextRequest={() =>
-            this.setState({
-              listIndex: (listIndex + 1) % galsList[imageIndex].length
-            })
-          }
-        />
+          <FsLightbox
+            mainSrc={galsList[imageIndex][listIndex]}
+            nextSrc={
+              galsList[imageIndex][
+                (listIndex + 1) % galsList[imageIndex].length
+              ]
+            }
+            prevSrc={
+              galsList[imageIndex][
+                (listIndex + galsList[imageIndex].length - 1) %
+                  galsList[imageIndex].length
+              ]
+            }
+            onCloseRequest={() => this.setState({ showLightbox: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                listIndex:
+                  (listIndex + galsList[imageIndex].length - 1) %
+                  galsList[imageIndex].length,
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                listIndex: (listIndex + 1) % galsList[imageIndex].length,
+              })
+            }
+          />
         )}
       </Fragment>
-    );
+    )
   }
-  
 }
--- src/components/quote-contacts.js
+++ src/components/quote-contacts.js
@@ -3,90 +3,82 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import styled from "styled-components"
 
 const Wrapper = styled.div`
-    @media (max-width:1150px) {
-        min-height: 200px;
-        padding-left: 1.2rem;
-    }
-    padding: 2.4rem 0 .8rem 2.4rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    border-bottom: solid 1px hsla(210,20%,25%,.98);
-    height: 33.33%;
-    > a {
-        text-decoration: none;
-    }
+  @media (max-width: 1150px) {
+    min-height: 200px;
+    padding-left: 1.2rem;
+  }
+  padding: 2.4rem 0 0.8rem 2.4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-bottom: solid 1px hsla(210, 20%, 25%, 0.98);
+  height: 33.33%;
+  > a {
+    text-decoration: none;
+  }
 `
 
-
 const ContactItemWrap = styled.div`
-    
+  text-decoration: none;
+  > a {
+    display: inline-flex;
+    font-family: Roboto;
+    font-weight: 300;
     text-decoration: none;
-    > a {
+    color: hsla(210, 73%, 95%, 0.8);
 
-        display: inline-flex;
-        font-family: Rubik;
-        text-decoration: none;
-        color: hsla(210,73%,95%,.8);
-
-        
-        &:hover {
-            > svg, p {
-                transition: color 0.5s;
-                color: rgba(128, 193, 255, 0.98);
-            }
-        }
-        
-        
-
-        > p {
-            padding: .2rem .6rem 0 .6rem;
-            
-        }
+    &:hover {
+      > svg,
+      p {
+        transition: color 0.5s;
+        color: rgba(128, 193, 255, 0.98);
+      }
     }
+
+    > p {
+      padding: 0.2rem 0.6rem 0 0.6rem;
+    }
+  }
 `
 
 const Icon = styled(FontAwesomeIcon)`
-    padding: .1rem;
-    font-size: 1.8rem;
-    color: hsla(210,73%,95%,.9);
+  padding: 0.1rem;
+  font-size: 1.8rem;
+  color: hsla(210, 73%, 95%, 0.9);
 `
 
-
 const QuoteContacts = () => (
-    <>
-        
-        <Wrapper>
-            
-            <ContactItemWrap>
-            <a href="tel:1-323-870-7046">
-                <Icon icon={["fas", "phone-alt"]} />
-                <p>(323) 870-7046</p>
-            </a> 
-            </ContactItemWrap>
-            
-        </Wrapper>
-      
-        <Wrapper>
-        
-            <ContactItemWrap>
-            <a href="mailto:calvin.selahroofing@gmail.com ">
-                <Icon icon={["fas", "envelope"]} />
-                <p>selahroofingcorp@gmail.com</p>
-            </a>
-            </ContactItemWrap>
-        </Wrapper>
-        <Wrapper>
-            <ContactItemWrap>
-            <a rel="noopener noreferrer" target="_blank" href="https://www.google.com/maps/place/Selah+Roofing/@33.9408414,-118.0954894,10z/data=!4m5!3m4!1s0x0:0x3b64bd9b48b72dd4!8m2!3d33.9408414!4d-118.0954894">
-                <Icon icon={["fas", "map-marker-alt"]} />
-                <p className="offset">Los Angeles, CA</p>
-            </a>
-            </ContactItemWrap>
-            
-        </Wrapper>
-        
-    </>
+  <>
+    <Wrapper>
+      <ContactItemWrap>
+        <a href="tel:1-323-870-7046">
+          <Icon icon={["fas", "phone-alt"]} />
+          <p>(323) 870-7046</p>
+        </a>
+      </ContactItemWrap>
+    </Wrapper>
+
+    <Wrapper>
+      <ContactItemWrap>
+        <a href="mailto:calvin.selahroofing@gmail.com ">
+          <Icon icon={["fas", "envelope"]} />
+          <p>selahroofingcorp@gmail.com</p>
+        </a>
+      </ContactItemWrap>
+    </Wrapper>
+    <Wrapper>
+      <ContactItemWrap>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.google.com/maps/place/Selah+Roofing/@33.9408414,-118.0954894,10z/data=!4m5!3m4!1s0x0:0x3b64bd9b48b72dd4!8m2!3d33.9408414!4d-118.0954894"
+        >
+          <Icon icon={["fas", "map-marker-alt"]} />
+          <p className="offset">Los Angeles, CA</p>
+        </a>
+      </ContactItemWrap>
+    </Wrapper>
+  </>
 )
 
 export default QuoteContacts
